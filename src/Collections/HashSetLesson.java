60266abce82378503e7b1b02e3978793ce683759
Elbek <elbekturdiyev20020707@gmail.com>
2024-06-01 13:06:35 +0500

    O'zgarishlar: HashSet class yakunlandi

--- src/Collections/HashSetLesson.java
+++ src/Collections/HashSetLesson.java
@@ -1,4 +1,17 @@
 package Collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetLesson {
+    public static void main(String[] args) {
+        Set<String> aa = new HashSet<>();
+
+        aa.add("Olama");
+        aa.add("Olama2");
+        aa.add("Olama3");
+        aa.add("Olama4");
+
+
+    }
 }
