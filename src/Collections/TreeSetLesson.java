1ff8131f857bf5aa181afdaee626e7e764990691
Elbek <elbekturdiyev20020707@gmail.com>
2024-06-02 16:42:36 +0500

    ...

--- src/Collections/TreeSetLesson.java
+++ src/Collections/TreeSetLesson.java
@@ -4,12 +4,7 @@ import java.util.TreeSet;
 
 public class TreeSetLesson {
     public static void main(String[] args) {
-        TreeSet treeSet = new TreeSet();
+        TreeSet<String> treeSet = new TreeSet<>();
 
-        treeSet.add(new Users(1, "Elbek"));
-        treeSet.add(new Users(2, "djh "));
-        treeSet.add(new Users(3, "adfv"));
-        treeSet.add(new Users(4, "adfv"));
-        System.out.println(treeSet);
     }
 }
