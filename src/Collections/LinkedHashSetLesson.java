45bd339837235e746ef0769088109a8da4869712
Elbek <elbekturdiyev20020707@gmail.com>
2024-06-02 17:01:34 +0500

    ...

---
+++ src/Collections/LinkedHashSetLesson.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetLesson {
+    public static void main(String[] args) {
+        LinkedHashSet<String> aa = new LinkedHashSet<>();
+
+        aa.add("srf");
+        aa.add("dfsv");
+        aa.add("retg");
+        aa.add(null);
+        aa.add("aaa");
+
+        System.out.println(aa);
+
+    }
+}
